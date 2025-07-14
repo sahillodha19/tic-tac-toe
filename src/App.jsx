1ebf2c0348027b462865b65ff6625c1ee1863918
sahillodha19 <sahillodha170@yahoo.com>
2025-07-15 01:07:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  )
+}
+
+export default App
